21ff0b5f7d79a58f71973854713858939cc2b857
Akusem <nathanael.z@outlook.fr>
2022-03-24 14:41:23 +0100

    feat: Change iDEP version in  start-idep.sh

--- start-idep.sh
+++ start-idep.sh
@@ -1,4 +1,4 @@
 export SINGULARITY_TMPDIR="/mnt/data/"
 export SINGULARITY_BIND="./shinyapps/:/srv/shiny-server/,./data/:/srv/data/,./countsData/:/srv/countsData/,./shinylog/:/var/log/shiny-server/,./config/:/etc/shiny-server/,./classes/:/usr/local/src/myscripts/"
 
-singularity run --env DOB_URL='http://localhost:3000/' idep.sif R -e "library('shiny');runApp('/srv/shiny-server/idep94/', port=9000)"
+singularity run --env DOB_URL='http://localhost:3000/' idep.sif R -e "library('shiny');runApp('/srv/shiny-server/idep95/', port=9000)"
